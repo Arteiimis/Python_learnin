378c73f1b537d0f66a21e393d3a23918e20d191f
Arteiimis <lionelym0987@gmail.com>
2022-05-04 17:57:59 +0800

    排序算法

---
+++ 排序算法总结/快速排序.py
@@ -0,0 +1,23 @@
+def QuickSort(arr, left = None, right = None):
+    left = 0 if not isinstance(left, (int, float)) else left
+    right = len(arr) - 1 if not isinstance(right, (int, float)) else right
+    if left < right:
+        partition_index = Partition(arr, left, right)
+        QuickSort(arr, left, partition_index - 1)
+        QuickSort(arr, partition_index + 1, right)
+    return arr
+
+def Partition(arr, left, right):
+    base = left
+    index = base + 1
+    i = index
+    while i <= right:
+        if arr[i] < arr[base]:
+            swap(arr, i, index)
+            insex += 1
+        i += 1
+    swap(arr, base, index - 1)
+    return index - 1
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
